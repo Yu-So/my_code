10a4a597a7b4561571ee3b328230a07af1113393
Yu-So <gluck_3d@mail.ru>
2019-12-17 21:05:11 +0400

    First GIT

--- excel_test.py
+++ excel_test.py
@@ -27,22 +27,30 @@ for tmp_file in xls_files:
         end_column = ws.max_column + 1
 
         for i in range(1,end_row):
+            occurrence_number = 0 # Количество столбцов в которых встречаются значения из словаря
             for j in range(1, end_column):
-                temp_value = str(ws.cell(row=i, column=j).value)
+                cell_value = str(ws.cell(row=i, column=j).value) # Значение ячейки
+                cell_name =  str(ws.cell(row=i, column=j))       # Наименование ячейки
 
+                dict_number = 0 # Количество встречающихся слов из словаря
 
                 for dict_line_tmp in dict_list:
                     ####
-                    if temp_value.lower().find(dict_line_tmp) != -1:
-                        ws.cell(row=i, column=end_column).value = "Нашел !!!"
-                        ws.cell(row=i, column=end_column+1).value = dict_line_tmp
-                    # Записывать значение никуда не нужно, оно ведь уже содержится в ws
+                    if cell_value.lower().find(dict_line_tmp) != -1:
+                        if dict_number == 0:
+                            ws.cell(row=i, column=end_column).value = "Нашел !!!"
+                            ws.cell(row=i, column=end_column + occurrence_number + 1).value = cell_name
+                            ws.cell(row=i, column=end_column + occurrence_number + 2).value = dict_line_tmp
+                            dict_number = 1 # Выставляем признак того что первое слово словаря найдено
+                        else:
+
+                            ws.cell(row=i, column=end_column + occurrence_number + 1).value = cell_name
+                            ws.cell(row=i, column=end_column + occurrence_number + 2).value = str(ws.cell(row=i, column=end_column + occurrence_number + 2).value) + ", \n" +dict_line_tmp
+                if dict_number == 1 : occurrence_number += 2 # Сдвигаем запись найденных слов словаря на 2 в право. Происходит только если в текущей ячейке было найдено хотя бы одно значение из словаря.
 
 
                 print (tmp_file," ", wb[k]," ", i," ",j)
 
-                # ws.cell(row=i, column=j).fill = PatternFill(fill_type='solid', start_color='ff0000', end_color='ff0000')
-                # Проверить и подсветить в искодном файле
 
     just_first_part_of_file_name = tmp_file.split(".xlsx")[0]
 
